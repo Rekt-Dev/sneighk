b39fd8f7d9a431b0dfae0b2771312a72623be8c8
Rekt-Dev <rektdevvv@gmail.com>
2022-06-21 11:17:36 +0300

    1 file modified

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let canvas = document.getElementById('canvas');
 
 let ROWS = 50;
 let COLS = 30;
+
 function initalizeCanvas() {
   for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < COLS; j++) {
