306e20ad7fe7d9d84c5f7da902636a358fa7079d
Rekt-Dev <rektdevvv@gmail.com>
2022-06-21 11:10:35 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// Import stylesheets
 import './style.css';
 
-// Write Javascript code!
 const appDiv = document.getElementById('app');
 
 let canvas = document.getElementById('canvas');
@@ -9,9 +7,10 @@ let canvas = document.getElementById('canvas');
 let ROWS = 50;
 let COLS = 30;
 function initalizeCanvas() {
-  for(let i=0;i<ROWS;i++){
-    for (let j=0;j<COLS;j++)
-    let pixel=document.createElement("div")
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLS; j++) {
+      let pixel = document.createElement('div');
+    }
   }
 }
 initalizeCanvas();
