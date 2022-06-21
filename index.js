ae3f02958e857f432047e4ac621a29f25e73236a
Rekt-Dev <rektdevvv@gmail.com>
2022-06-21 12:17:15 +0300

    1 file modified

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ let canvas = document.getElementById('canvas');
 
 let ROWS = 50;
 let COLS = 30;
+let PIXEL = 10;
 
 function initalizeCanvas() {
   for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < COLS; j++) {
       let pixel = document.createElement('div');
+      pixel.style.position = 'absolute';
+      pixel.style.border = `1px solid red`;
+      pixel.style.left = j * PIXEL + `px`;
+      pixel.style.top = i * PIXEL + `px`;
+
+      pixel.style.width = PIXEL + `px`;
+      pixel.style.height = PIXEL + `px`;
+
+      canvas.appendChild(pixel);
     }
   }
 }
