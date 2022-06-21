b9eb5ce0eb6eeca330e04fbbc7eb4bb95a3c8968
Rekt-Dev <rektdevvv@gmail.com>
2022-06-21 10:42:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-iuu2sg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-iuu2sg)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+alert('sup brah');
+appDiv.innerHTML = `<h1>fahk a u </h1>`;
