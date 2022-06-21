9fed0751b3448d36a4b24e61dfb56407670a437e
Rekt-Dev <rektdevvv@gmail.com>
2022-06-21 11:04:09 +0300

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>fahk a u </h1>`;
+
+let canvas = DOCUMENT.getElementById('canvas');
+
+function initalizeCanvas() {}
+initalizeCanvas();
